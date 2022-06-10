7d986f262be6e41b6c0454acc422186a362d3dae
bHead_96 <chinghsiang2019@gmail.com>
2022-06-10 15:03:53 +0800

    test

--- web.js
+++ web.js
@@ -9,6 +9,24 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
+  console.log(req.body);
+  const moves = ['F', 'T', 'L', 'R', 'T', 'T', 'T', 'T', 'T', 'T', 'T', 'T'];
+  res.send(moves[Math.floor(Math.random() * moves.length)]);
+});
+
+app.listen(process.env.PORT || 8080);
+
+/* const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Let the battle begin!');
+});
+
+app.post('/', function (req, res) {
   //console.log(req.body);
   //console.log(req.body.arena);
   //console.log()
@@ -25,4 +43,4 @@ app.post('/', function (req, res) {
   }
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080); */
